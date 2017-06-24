189e85de41b013461e466e99bc9ffade454f8365
datfailkid <myronstewartjr@gmail.com>
2017-06-24 10:39:10 -0400

    Completed lesson 6.1

---
+++ function/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+//Function prototype
+void greetUser();
+void goodbye();
+
+int main() {
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    greetUser();
+    goodbye();
+    return 0;
+}
+
+void greetUser() {
+    printf("Hello!\n");
+}
+void goodbye() {
+    printf("Sorry, ran out of time, I've got to go!\n");
+}
