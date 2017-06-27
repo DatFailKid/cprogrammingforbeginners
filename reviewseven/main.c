7a00c9e87300dad298e54f27177184eb6621103b
datfailkid <myronstewartjr@gmail.com>
2017-06-27 10:19:45 -0400

    Changed the "space" function, added memory address.

--- reviewseven/main.c
+++ reviewseven/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-void space();
+void items();
 
 int yaboi=92264;
 char petDog[]="Jake";
 
 int main() {
     printf("I have a few belongings on this computer, and I'm going to tell you how much space they take up. Is this ok?");
-    scanf("%d");
-    space();
+    getchar();
+    items();
     return 0;
 }
 
-void space() {
+void items() {
     printf("\nI AM YA BOI, AND I TAKE UP %d BITES ON THIS COMPUTER", sizeof(yaboi));
+    printf(", AND MY MEMORY ADDRESS IS %u.", &yaboi);
     printf("\nI am Jake teh doggo and I take up %d bites on tis computer", sizeof(petDog));
+    printf(". I stays at %u.", &petDog);
 }
